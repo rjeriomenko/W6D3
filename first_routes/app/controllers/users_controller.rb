9fbda2d349cc40b9994871aa6f89521f73103bc9
harjitsingh3 <singh.harjit09@gmail.com>
2023-04-12 15:55:50 -0400

    Complete Phase 2

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController 
     def root
-        render json: [1]
+        #print "root?"
+        #render json: [1]
     end
+
+    def index
+        render plain: "You are in the user's resource"
+    end 
 end
--- first_routes/config/routes.rb
+++ first_routes/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#root"
+
+  get '/root/', to: 'users#root'
 end
