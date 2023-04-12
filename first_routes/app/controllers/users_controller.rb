fca679ff3d97194fe1d00f52ab05305b00a8a204
harjitsingh3 <singh.harjit09@gmail.com>
2023-04-12 17:25:52 -0400

    Finish Phase 3

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
     def index
         render plain: "You are in the user's resource"
     end 
+
+    def create
+        password1 = params[:password][:password1]
+        password2 = params[:password][:password2]
+        username = params[:username]
+        render json: [username, password1, password2]
+        #render plain: "#{params[:num].to_i * 10}"
+    end
+    
+    def show
+        if params[:id].to_i == 10
+        render plain: "Yes, the ID is #{params[:id]}"
+        else 
+        render plain: "No, the ID is actually #{params[:id]}"
+        end 
+
+    end
 end
+
+
+
